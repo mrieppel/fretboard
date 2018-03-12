fb1fd287bddd17d2ca9c788acae028e9861f2660
Michael Rieppel <mrieppel@gmail.com>
2018-03-12 16:28:40 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Fretboard
+=========
+
+Fretboard is a guitar theory app that lets users see where the notes corresponding to a given chord, or to a complete scale, occur along the length of a guitar's fretboard.  For chords, users can specify the root note, type of chord, and guitar tuning.  Similarly, for scales users can specify the root note, scale mode, and guitar tuning.  The fretboard is drawn using the [D3.js](http://d3js.org/) library.
+
+A live version is [here](http://mrieppel.github.io/fretboard/)
+
+Internally, the program represents notes as integers (0 for C, 1 for C#, 2 for D etc.).  Thus [enharmonic equivalents](https://en.wikipedia.org/wiki/Enharmonic), like C# and Db, are represented by the same integer.  One problem is to then recover the correct name for a note in the context of a given key (e.g. in the Bmaj scale, it's called "C#", but in the Fmin scale it's called Db). I'm not certain that the program solves this problem correctly in *all* cases (especially for chords).  If you come across bugs, please email at: mrieppel at gmail dot com.
---
+++ script.js
@@ -0,0 +1,268 @@
+// Fretboard
+//==============
+//
+// Copyright (c) 2018 Michael Rieppel
+//
+// Internally, the program identifies notes by integers (see the global NOTES variable)
+// rather than by their ordinary names, due to enharmonic equivalents which have two
+// ordinary names. E.g. the program uniformly uses '1' for what would ordinarily be called
+// either "C#" or "Db", depending on context.
+//
+// A scale is represented as an array of integers, each integer representing a note in
+// the scale.  E.g. Cmaj is [0,2,4,5,7,9,11].  Similarly for chords.  E.g. the Cmaj 
+// chord is represented as [0,4,7], corresponding to the 1st, 3rd, and 5th degrees of
+// the Cmaj scale.
+//
+// The function scaleDict then takes a scale in array form and returns a dictionary
+// that pairs integers in the array with ordinary note names, resolving between the two 
+// names of enharmonic equivalents as appropriate.  Such a dictionary can also be applied
+// to an array representing a chord to get the names of the notes in that chord.
+
+
+var NOTES = {"C":0,"B#":0,"C#":1,"Db":1,"D":2,"D#":3,"Eb":3,"E":4,"Fb":4,"E#":5,"F":5,"F#":6,"Gb":6,"G":7,"G#":8,"Ab":8,"A":9,"A#":10,"Bb":10,"B":11,"Cb":11}
+
+// Main function to collect user input and display the appropriate scale 
+function putScale() {
+	var tuning = document.getElementById('scaletuning').value.split(",").reverse();
+	var root = document.getElementById('scaleroot').value;
+	var m = document.getElementById('scalemode'); 
+	
+	var mode = m.options[m.selectedIndex].text; // name of mode
+	var modear = m.value.split(",").map(x => parseInt(x)); // interval array for mode
+	
+	var board = makeBoard(tuning);
+	
+	var fscale = makeScale(modear,NOTES[root]); // Full scale
+	var scale = getScaleFrag(fscale,mode); // Scale fragment for pentatonics etc.
+	
+	var boardnotes = findKeyNotes(board,scale);
+	var dict = scaleDict(fscale,root);
+	var boardnotes = putLetters(boardnotes,dict);
+	
+	var scalestr = scale.map(x => dict[x]).join(","); // String description of scale
+	d3.select("#scaleinfo").html(root+" "+mode+" Scale: "+scalestr);
+
+	drawopennotes("fretboard",tuning);
+	for(var i=0;i<boardnotes.length;i++) {
+		drawnotes("fretboard",i+1,boardnotes[i]);
+	}
+}
+
+
+// Takes a full scale (as an array of integers) and the name of a mode, and returns
+// the appropriate scale fragment (for pentatonic scales, blues etc.).  For full
+// diatonic scales, just returns the original scale
+function getScaleFrag(fscale,mode) {
+	switch(mode) {
+		case "Maj. Pentatonic" : return [fscale[0],fscale[1],fscale[2],fscale[4],fscale[5]];
+		case "Min. Pentatonic" : return [fscale[0],fscale[2],fscale[3],fscale[4],fscale[6]];
+		case "Blues" : return [fscale[0],fscale[2],fscale[3],fscale[4],fscale[5],fscale[7]];
+		default : return fscale;
+	
+	}
+
+}
+
+// Main function to collect user input and display the appropriate chord
+function putChord() {
+	var tuning = document.getElementById('chordtuning').value.split(",").reverse();
+	var root = document.getElementById('chordroot').value;
+	
+	var m = document.getElementById('chordmode'); 
+ 	var mode = m.options[m.selectedIndex].text;
+ 	var modear = m.value.split(",").map(x => parseInt(x));
+
+	var scale = makeScale(modear,NOTES[root]);
+	
+	var chord = getChord(scale,mode);
+	
+	var board = makeBoard(tuning);
+	var boardnotes = findKeyNotes(board,chord);
+	var dict = scaleDict(scale,root);
+	
+	var boardnotes = putLetters(boardnotes,dict);
+	
+	var chordstr = chord.map(x => dict[x]).join(",");
+	d3.select("#chordinfo").html(root+mode+" Chord: "+chordstr);
+	
+ 	drawopennotes("chordboard",tuning);
+ 	for(var i=0;i<boardnotes.length;i++) {
+ 		drawnotes("chordboard",i+1,boardnotes[i]);
+ 	}
+}
+
+// Takes a scale and the name of a "mode" (type of chord, e.g. "maj" or "dim7")
+// and returns the fragment of the scale representing the chord.
+function getChord(scale,mode) {
+	switch(mode) {
+		case "maj" : return [scale[0],scale[2],scale[4]];
+		case "min" : return [scale[0],scale[2],scale[4]];
+		case "7" : return [scale[0],scale[2],scale[4],scale[6]];
+		case "maj7" : return [scale[0],scale[2],scale[4],scale[6]];
+		case "min7" : return [scale[0],scale[2],scale[4],scale[6]];
+		case "maj6" : return [scale[0],scale[2],scale[4],scale[5]];
+		case "min6" : return [scale[0],scale[2],scale[4],scale[5]];
+		case "aug" : return [scale[0],scale[2],scale[4]];
+		case "dim" : return [scale[0],scale[2],scale[4]];
+		case "dim7" : return [scale[0],scale[2],scale[4],scale[6]];
+		case "sus4" : return [scale[0],scale[3],scale[4]];
+		default : return scale;
+	}
+}
+
+
+// Takes a scale (array of integers) and a root (e.g. "C"), and returns the appropriate 
+// names for the other notes in the scale (determines whether e.g. C#, represented as an
+// int 1 in the scale, should be called "C#" or "Db"
+function scaleDict(scale,root) {
+	var test = root;
+	var out = {};
+	out[scale[0]] = root;
+	for(var i=1;i<scale.length;i++) {
+		out[scale[i]] = getLet(scale[i],test);
+		test += ","+out[scale[i]];
+	}
+	return out;
+	
+	function getLet(n,str) {
+		switch(n) {
+			case 0 : return str.indexOf("C")<0 ? "C" : "B#";
+			case 1 : return str.indexOf("C")<0 ? "C#" : "Db";
+			case 2 : return "D";
+			case 3 : return str.indexOf("D")<0 ? "D#" : "Eb";
+			case 4 : return "E";
+			case 5 : return str.indexOf("F")<0 ? "F" : "E#";
+			case 6 : return str.indexOf("F")<0 ? "F#" : "Gb";
+			case 7 : return "G";
+			case 8 : return str.indexOf("G")<0 ? "G#" : "Ab";
+			case 9 : return "A";
+			case 10 : return str.indexOf("A")<0 ? "A#" : "Bb";
+			case 11 : return str.indexOf("B")<0 ? "B" : "Cb";		
+		}
+	}
+}
+
+
+// Takes a mode (an array specifying the intervals for the scale) and a root note 
+// (represented as integer) and returns the scale (array of integers) starting at the 
+// root and moving up by the specified intervals.
+function makeScale(mode,root) {
+		var scale = [root];
+		for(var i=0;i<(mode.length-1);i++) {
+			root += mode[i];
+			scale.push(root%12);
+		}
+	return scale;
+}
+
+// Takes a full fretboard (each fret labeled with the note it corresponds to) and a scale
+// (as an array of ints) and removes those frets from the fretboard that don't correspond
+// to notes in the given scale
+function findKeyNotes(board,scale) {
+	return board.map(function (s) {
+		return s.filter(function (p) {
+			return scale.indexOf(p[1]) >=0;});});
+}
+
+// Takes a tuning and returns a full fretboard based on that tuning, with each fret
+// assigned the note it corresponds to on the given tuning.  A fretboard is a 
+// multidimensional array: one array for each of the 6 strings, where each of those
+// arrays in turn consists of 14 arrays representing the 14 frets on that string.  Each
+// of these "fret arrays" contains two integers: the first for the fret number, the 
+// second for the note (as integer) that corresponds to that fret on the tuning.
+function makeBoard(tuning) {
+	var board = [];
+	var string = [];
+	for(var i=0;i<6;i++) {
+		var note = NOTES[tuning[i]];
+		for(var j=0;j<15;j++) {
+			string.push([j,(note%12)]);
+			note++;
+		}
+		board.push(string);
+		string = [];
+	}
+	return board;
+}
+
+
+// Takes a fretboard and a dictionary and labels assigns to each fret the ordinary note 
+// that corresponds to it. (Frets are originally labeled with the integer name of the
+// note, and this function replaces that integer with the ordinary name of the note.)
+function putLetters(board,dict) {
+	return board.map(function(nested) {
+		return nested.map(function(i) {
+			return [i[0],dict[i[1]]];
+		});});
+}
+
+
+
+// BELOW ARE FUNCTIONS TO DRAW TO SVG USING D3
+
+
+// Draws the notes each of the 6 strings is tuned to to the left of that string.
+function drawopennotes(id,tuning) {
+	var st = d3.select("#"+id).selectAll("g.string");
+	var x = st.data(tuning);
+	var y = x.selectAll("text.opennote").data(d => [d]);
+	y.text(d => d);
+	y.enter()
+		.append("text")
+		.attr("x",25)
+		.attr("y",5)
+		.attr("class","opennote")
+		.style("font-family","Verdana")
+		.style("font-size","14")
+		.text(function (d) {return d;});
+	
+	y.exit().remove();
+}
+
+
+// Draws the notes corresponding to the given scale or chord onto the fretboard.
+function drawnotes(id,string,frets) {
+	var st = d3.select("#"+id).select("#s"+string);
+
+	var c = st.selectAll("circle.note").data(frets)
+		.attr("r",11)
+		.attr("cx", d => d[0]==0 ? 35 : (d[0]*50)+30)
+		.attr("cy", 0)
+		.attr("class","note")
+		.style("stroke","black")
+		.style("stroke-width","1")
+		.style("fill","RGB(247,148,89)")
+		.style("fill-opacity","1");
+	
+	c.enter()
+		.append("circle")
+		.attr("r",11)
+		.attr("cx", d => d[0]==0 ? 35 : (d[0]*50)+30)
+		.attr("cy", 0)
+		.attr("class","note")
+		.style("stroke","black")
+		.style("stroke-width","1")
+		.style("fill","RGB(247,148,89)")
+		.style("fill-opacity","1");
+	
+	c.exit().remove();
+
+	var t = st.selectAll("text.note").data(frets)
+		.attr("x", d => d[0]==0 ? 28 : (d[0]*50)+23)
+		.attr("y", 4)
+		.attr("class","note")
+		.style("font-family","Verdana")
+		.style("font-size","11")
+		.text(d => d[1]);
+		
+	t.data(frets).enter()
+		.append("text")
+		.attr("x", d => d[0]==0 ? 28 : (d[0]*50)+23)
+		.attr("y", 4)
+		.attr("class","note")
+		.style("font-family","Verdana")
+		.style("font-size","11")
+		.text(d => d[1]);
+		
+	t.exit().remove();
+}
